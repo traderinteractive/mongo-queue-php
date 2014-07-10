eac41c4db32f263420733a40088f61aaff562703
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-07-09 20:59:41 -0400

    Create a docker-based build.
    This helps make it easier to run this build by removing the necessity to
    setup and run a local mongo server.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM base/archlinux
+
+RUN pacman --sync --refresh --noconfirm --noprogressbar --quiet
+RUN pacman --sync --noconfirm --noprogressbar --quiet php xdebug php-mongo git openssh
+
+ADD provisioning/php/php-extensions.ini /etc/php/conf.d/extensions.ini
+ADD provisioning/php/xdebug.ini /etc/php/conf.d/xdebug.ini
+ADD provisioning/set-env.sh /set-env.sh
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+VOLUME ["/code"]
+
+ENTRYPOINT ["/set-env.sh"]
+CMD ["/code/build.php"]
---
+++ provisioning/set-env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if test -z "${MONGO_PORT_27017_TCP_ADDR}" -o -z "${MONGO_PORT_27017_TCP_PORT}"; then
+    echo "You must link this container with mongo first"
+    exit 1
+fi
+
+export TESTING_MONGO_URL="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}"
+
+# See http://tldp.org/LDP/abs/html/devref1.html for description of this syntax.
+while ! exec 6<>/dev/tcp/${MONGO_PORT_27017_TCP_ADDR}/${MONGO_PORT_27017_TCP_PORT}; do
+    echo "$(date) - still trying to connect to mongo at ${TESTING_MONGO_URL}"
+    sleep 1
+done
+
+exec 6>&-
+exec 6<&-
+
+$@
