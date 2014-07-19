9d5daca64d699d608673fe3f2962d582d6911e59
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-07-18 20:28:35 -0400

    Use phpunit as a library.
    This enables more programmatic access to phpunit, especially when it
    comes to code coverage.  By using the code coverage report object
    directly we can remove the clover.xml generation and libxml dependency.

--- build.php
+++ build.php
@@ -21,17 +21,19 @@ if ($phpcsViolations > 0) {
     exit(1);
 }
 
-passthru('vendor/bin/phpunit --coverage-html coverage --coverage-clover clover.xml', $returnStatus);
-if ($returnStatus !== 0) {
+$phpunitConfiguration = PHPUnit_Util_Configuration::getInstance(__DIR__ . '/phpunit.xml');
+$phpunitArguments = array('coverageHtml' => __DIR__ . '/coverage', 'configuration' => $phpunitConfiguration);
+$testRunner = new PHPUnit_TextUI_TestRunner();
+$result = $testRunner->doRun($phpunitConfiguration->getTestSuiteConfiguration(), $phpunitArguments);
+if (!$result->wasSuccessful()) {
     exit(1);
 }
 
-$xml = new SimpleXMLElement(file_get_contents('clover.xml'));
-foreach ($xml->xpath('//file/metrics') as $metric) {
-    if ((int)$metric['elements'] !== (int)$metric['coveredelements']) {
-        file_put_contents('php://stderr', "Code coverage was NOT 100%\n");
-        exit(1);
-    }
+$coverageFactory = new PHP_CodeCoverage_Report_Factory();
+$coverageReport = $coverageFactory->create($result->getCodeCoverage());
+if ($coverageReport->getNumExecutedLines() !== $coverageReport->getNumExecutableLines()) {
+    file_put_contents('php://stderr', "Code coverage was NOT 100%\n");
+    exit(1);
 }
 
 echo "Code coverage was 100%\n";
