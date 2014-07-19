e714dc8d184a97908668e58bfd4b120cbd35fd10
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-07-18 20:26:12 -0400

    Use phpcs as a library.
    Rather than executing phpcs via passthru, pull it in as a library.  This
    enables more programmatic access to it.

--- build.php
+++ build.php
@@ -8,8 +8,16 @@ if ($returnStatus !== 0) {
     exit(1);
 }
 
-passthru('./vendor/bin/phpcs --standard=' . __DIR__ . '/vendor/dominionenterprises/dws-coding-standard/DWS -n src tests *.php', $returnStatus);
-if ($returnStatus !== 0) {
+require 'vendor/autoload.php';
+
+$phpcsCLI = new PHP_CodeSniffer_CLI();
+$phpcsArguments = array(
+    'standard' => array(__DIR__ . '/vendor/dominionenterprises/dws-coding-standard/DWS'),
+    'files' => array('src', 'tests', 'build.php'),
+    'warningSeverity' => 0,
+);
+$phpcsViolations = $phpcsCLI->process($phpcsArguments);
+if ($phpcsViolations > 0) {
     exit(1);
 }
 
